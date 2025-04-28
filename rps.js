8e4e12c2cf58d7fee1b76805c6d66ed43f6b9034
nsw11 <nswen@ucsc.edu>
2025-04-28 12:44:15 -0700

    added ui functionality and (mostly) moved interaction away from command line

--- rps.js
+++ rps.js
@@ -3,29 +3,27 @@ function getComputerChoice()
 {
     return Math.floor(Math.random() * 3);
 }
-function getUserChoice()
+
+function getUserChoice(choice)
 {
-    while(true)
+    if (choice== "rock")
     {
-        let choice = prompt("Pick:Rock,Paper,or Scissors!\n");
-        if (choice.toLowerCase()== "rock")
-        {
-            return 0;
-        }
-        else if (choice.toLowerCase()== "paper")
-        {
-            return 1; 
-        }
-        else if (choice.toLowerCase()== "scissors")
-        {
-            return 2;
-        }
-        else
-        {
-            console.log("Invalid Input! Please Try again\n")
-        }
+        return 0;
+    }
+    else if (choice== "paper")
+    {
+        return 1; 
+    }
+    else if (choice== "scissors")
+    {
+        return 2;
+    }
+    else
+    {
+        console.log("Invalid Input! Please Try again\n")
     }
 }
+
 function parse_value(value)
 {
     if (value == 0)
@@ -46,69 +44,71 @@ function parse_value(value)
     }
 }
 
-function play_round()
+function play_round(UserChoice)
 {
+    if (UserScore >= 5 || CompScore>=5)
+        {
+            return
+        }
+//Generate Computer Choice
     let ComputerChoice = getComputerChoice();
-    console.log("The Computer picked: "+parse_value(ComputerChoice))
-    
-    let UserChoice = getUserChoice();
-    console.log("You picked: "+parse_value(UserChoice))
-
-    
+//List Choices for round processing
+    const PlayerResult = document.querySelector("#PlayerChoice");
+    PlayerResult.textContent= "You Chose: " + parse_value(UserChoice);
+    const CompResult = document.querySelector("#CompChoice");
+    CompResult.textContent= "Computer Chose: " + parse_value(ComputerChoice);
+    const results = document.querySelector("#result");
+//Determine Winner + Update Global Variable
     if ((ComputerChoice == 2 & UserChoice == 0)) //User Wins
     {
-        console.log("You win this round!\n")
-        return 1
+        results.textContent = "\t You win the round!";
+        ++UserScore;
     }
     else if ((ComputerChoice == 0 & UserChoice == 2)) //Computer Wins
     {
-        console.log("The computer win this round!\n")
-        return -1
+        results.textContent = "\t The computer win this round!";
+        ++CompScore;
     }
     else if (UserChoice>ComputerChoice)
     {
-        console.log("You win this round!\n")
-        return 1 
+        results.textContent = "\t You win the round!";
+        ++UserScore;
     }
     else if (UserChoice<ComputerChoice)
     {
-        console.log("The computer win this round!\n")
-        return -1
+        results.textContent = "\t The computer win this round!";
+        ++CompScore;
     }
     else
     {
-        console.log("The round is a tie!\n");
-        return 0
+        results.textContent = "\t This round is a tie!";
+    }
+    
+//Update Scoreboard
+const User_scoreboard = document.querySelector("#UserScore");
+const Comp_scoreboard = document.querySelector("#CompScore");
+User_scoreboard.textContent = UserScore;
+Comp_scoreboard.textContent = CompScore;
+//Describe Round
+if (UserScore >= 5)
+    {
+        const match_result = document.createElement("div");
+        match_result.textContent = "User wins the game!";
+        const final_result = document.querySelector("#LastResult");
+        final_result.appendChild(match_result);
+    }
+    else if (CompScore>=5)
+    {
+        const match_result = document.createElement("div");
+        match_result.textContent = "Computer wins the game!";
+        const final_result = document.querySelector("#LastResult");
+        final_result.appendChild(match_result);
     }
 }
+let UserScore = 0;
+let CompScore = 0;
+let play = document.querySelector("#buttons");
+play.addEventListener('click',(event)=>{
+    play_round(getUserChoice(event.target.id));
+})
 
-function play_game()
-{
-    console.log("If I beat you 3 times at rock paper scissors, I take your stand power!\n")
-    let UserScore = 0
-    let ComputerScore = 0;
-    while (UserScore < 3 & ComputerScore<3)
-    {
-        let result = play_round()
-        if (result==1)
-        {
-            ++UserScore;
-        }
-        else if (result==-1)
-        {
-            ++ComputerScore;
-        }
-        console.log("Current User Score: "+ UserScore+ "\n");
-        console.log("Current Computer Score: "+ ComputerScore+ "\n");
-    }
-    if (UserScore>ComputerScore)
-    {
-        console.log("You Win!");
-    }
-    else
-    {
-        console.log("You Lose. Wah Wah!");
-    }
-}
-
-play_game();
